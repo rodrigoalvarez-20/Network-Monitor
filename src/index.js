c066ab99db5f8682401aa1f4d5fad09dcfaea2b7
ralvarez20 <rodrigoalvarez449@gmail.com>
2022-05-20 02:36:03 -0500

    .

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { GlobalStateProvider } from './utils/globalContext';
 import axios from 'axios';
 
-//axios.defaults.baseURL = "http://127.0.0.1:5000"
+axios.defaults.baseURL = "http://20.225.148.220"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
