e98b214c96529eadfc8f9508c20e49be2fbb0724
ralvarez20 <rodrigoalvarez449@gmail.com>
2022-05-20 01:08:51 -0500

    .

--- src/index.js
+++ src/index.js
@@ -5,9 +5,6 @@ import { BrowserRouter } from 'react-router-dom';
 import { CookiesProvider } from 'react-cookie';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { GlobalStateProvider } from './utils/globalContext';
-import axios from 'axios';
-
-axios.defaults.baseURL = "https://asr-network-api.herokuapp.com"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
